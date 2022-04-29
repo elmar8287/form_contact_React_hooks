6eb9438655b342a396441fd1f94e2a9befdff071
Elmar <L-mar@inbox.ru>
2022-04-29 23:54:28 +0400

    Fix linters

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import "./Form.css";
+import './Form.css';
 
 function Form() {
   return (
-    <div className='main'>
-      <form className='form'>
-        <input type="text" placeholder="First name" maxLength="50" required/>
-        <input type="text" placeholder="Last name" maxLength="50" required/>
-        <input type="email" placeholder="Email" maxLength="50" required/>
-        <textarea placeholder="Message" maxLength="50"/>
-        <button type="submit" className='form-btn'>Send</button>
+    <div className="main">
+      <form className="form">
+        <input type="text" placeholder="First name" maxLength="20" required />
+        <input type="text" placeholder="Last name" maxLength="20" required />
+        <input type="email" placeholder="Email" maxLength="20" required />
+        <textarea placeholder="Message" maxLength="50" />
+        <button type="submit" className="form-btn">Send</button>
       </form>
     </div>
   );
