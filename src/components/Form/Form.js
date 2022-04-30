131795e01df12361192fe5148f98ae8720d88627
Elmar <L-mar@inbox.ru>
2022-04-30 14:36:19 +0400

    Create a state for inputs useState hook

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Form.css';
 
 function Form() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    textarea: "",
+  })
+
+  const inputHandle = (e) => {
+    e.preventDefault();
+    setFormData(e.target.value)
+  }
   return (
     <div className="main">
       <form className="form">
-        <input type="text" placeholder="First name" maxLength="20" required />
-        <input type="text" placeholder="Last name" maxLength="20" required />
-        <input type="email" placeholder="Email" maxLength="20" required />
-        <textarea placeholder="Message" maxLength="50" />
+        <input
+          type="text"
+          value={formData.firstName}
+          onChange={inputHandle}
+          placeholder="First name"
+          maxLength="20"
+          required
+        />
+        <input
+          type="text"
+          value={formData.lastName}
+          onChange={inputHandle}
+          placeholder="Last name"
+          maxLength="20"
+          required
+        />
+        <input
+          type="email"
+          value={formData.email}
+          onChange={inputHandle}
+          placeholder="Email"
+          maxLength="20"
+          required
+        />
+        <textarea
+          value={formData.textarea}
+          onChange={inputHandle}
+          placeholder="Message"
+          maxLength="20"
+          required
+        />
         <button type="submit" className="form-btn">Send</button>
       </form>
       <div className="success">Succesfully sent</div>
